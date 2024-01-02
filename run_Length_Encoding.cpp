af8951a17757b0bf9f42ea694e62baa34dfc6c7b
sidjain8427 <91948606+sidjain8427@users.noreply.github.com>
2024-01-03 03:42:15 +0530

    Update and rename run_Length_Encoding to run_Length_Encoding.cpp
    added the comments and provided the problem link for easy accessing the problem..

---
+++ run_Length_Encoding.cpp
@@ -0,0 +1,23 @@
+/*You are required to complete this function */
+// Please include the header files and also namespace and also the main function if you want to run in an editor in your local machine 
+// This code only contains the function only and if you want to run it , Please copy and paste it on the geeksforgeeks platoform cpp compiler on the given question.
+// I am providing the quesLink here..
+// https://www.geeksforgeeks.org/problems/run-length-encoding/1
+string encode(string src)
+{     
+    int n = src.length();
+    string ans = "";
+
+    for(int i = 0; i < n; i++) {
+      int count = 1;
+      ans += src[i];
+      while(i < n-1 && src[i] == src[i+1]) {
+          count++;
+          i++;
+          
+      }
+      ans += to_string(count);
+        
+    }
+  return ans;
+}     
